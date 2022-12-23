c61e40fb3bf0421c75f5a012452fd9d6f4baab2b
Michael McLane <mclane.a.michael@gmail.com>
2022-12-22 20:47:01 -0600

    user models

---
+++ migrations/create_users_table.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+
+
+
+
+        await queryInterface.createTable('users', { 
+            id:{
+            type:Sequelize.INTEGER(11),
+            allowNull:false,
+            primaryKey:true,
+            autoIncrement:true
+        },
+        name:{
+            type:Sequelize.STRING,
+            allowNull:false,
+            unique:true,
+        },
+        email:{
+            type:Sequelize.STRING,
+            allowNull:false,
+            unique:true,
+        },
+        phone:{
+            type:Sequelize.STRING,
+            allowNull:false,
+            unique:true,
+        },
+        createdAt:Sequelize.DATE,
+        updatedAt:Sequelize.DATE
+     })
+
+    },
+
+    down: async (queryInterface, Sequelize) => {
+
+        await queryInterface.dropTable('users');
+    }
+};

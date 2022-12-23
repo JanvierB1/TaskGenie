bf56c0aacf47694d16d2571eeb79f4f764c6d671
uhdznataly <112514096+uhdznataly@users.noreply.github.com>
2022-12-22 20:31:44 -0600

    added createUser.hbs, editPage.hbs, user.hbs, src folder with controllers, models, routes folders. added require hbs to app.js

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const app = express();
-const port = process.env.PORT|| 5000;
+const port = process.env.PORT|| 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
+app.engine('hbs', exphbs({extname:'.hbs'}));
+app.search('view engine', 'hbs');
+
+app.get('/', (req, res)=>{
+    res.render('homePage');
+
+})
 
 app.listen(port, ()=>{
-    console.log('The server is listening to port ${port}')
-})
+    console.log(`The server is listening to port ${port}`)
+});
